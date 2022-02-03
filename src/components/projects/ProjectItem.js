30f660d4da4049bd3738984cf6b9edee2df7ef05
Virus936 <lok.vira@gmail.com>
2022-02-03 17:23:59 +0100

    Projects, ProjectItems components

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -1,9 +1,76 @@
+import styled from 'styled-components'
+import {size} from '../../settings'
+
 const ProjectItem = ({img, title, desc}) => {
-  return <div>
+  return <Container>
     <img src={img} alt={img} />
-    <h1>{title}</h1>
-    <p>{desc}</p>
+    <div>
+      <h1>{title}</h1>
+      <p>{desc}</p>
     </div>
+  </Container>
 }
+
+const Container = styled.div`
+  display:flex;
+  flex-direction:column;
+  flex:100%;
+  background:#fdf4f2;
+  border-radius:var(--border-radius);
+  overflow:hidden;
+  &:hover{
+    background:var(--color-peach);
+    color:white;
+    transition:.2s;
+    h1{
+      color:white;
+      transition:.2s;
+    }
+  }
+  img{
+    display:block;
+    background-color:dodgerblue;
+    width:100%;
+    aspect-ratio:1;
+  }
+  div{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    text-align:center;
+    padding:1em;
+    *{
+      max-width:300px;
+    }
+
+    h1{
+      text-transform:uppercase;
+      letter-spacing:.15em;
+      color:var(--color-peach);
+      font-size:1.3em;
+    }
+    p{
+      line-height:2em;
+      font-size:small;
+    }
+  }
+  @media(min-width:470px )and (max-width:${size.medium}){
+    flex-direction:row;
+
+    img{
+      order:2;
+      flex:1;
+      width:300px;
+    }
+    div{
+      flex:1;
+    }
+  }
+  @media(min-width:${size.medium}){
+    flex:270px;
+  }
+
+`
 
 export default ProjectItem
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,41 +1,111 @@
 import ProjectItem from './ProjectItem'
+import {useParams} from 'react-router-dom'
+import styled from 'styled-components'
+
 let projects = [
   {
+    tag:'web',
     img:'image',
     title:'express',
     desc:'A multi-carrier shipping website for ecommerce businesses'
   },
   {
+    tag:'web',
     img:'image',
     title:'transfer',
     desc:'Site for low-cost money transfers and sending money within seconds '
   },
   {
+    tag:'web',
     img:'photom',
     title:'photon',
     desc:'A state-of-the-art music player with high-resolution audio and DSP effects '
   },
   {
+    tag:'web',
     img:'image',
     title:'builder',
     desc:'Connects users with local contractors based on their location'
   },
   {
+    tag:'web',
     img:'image',
     title:'blogr',
     desc:'Blogr is a platform for creating an online blog or publication'
   },
   {
+    tag:'web',
     img:'image',
     title:'camp',
     desc:'Get expert training in coding, data, design, and digital marketing'
   },
+  {
+    tag:'graphic',
+    img:'image',
+    title:'tim brown',
+    desc:'A book cover designed for Tim Brown’s new release, ‘Change’'
+  },
+  {
+    tag:'graphic',
+    img:'image',
+    title:'boxed water',
+    desc:'A simple packaging concept made for Boxed Water'
+  },
+  {
+    tag:'graphic',
+    img:'image',
+    title:'science!',
+    desc:'A poster made in collaboration with the Federal Art Project'
+  },
+  {
+    tag:'app',
+    img:'image',
+    title:'airfilter',
+    desc:'Solving the problem of poor indoor air quality by filtering the air '
+  },
+  {
+    tag:'app',
+    img:'image',
+    title:'eyecam',
+    desc:'Product that lets you edit your favorite photos and videos at any time'
+  },
+  {
+    tag:'app',
+    img:'image',
+    title:'faceit',
+    desc:'Get to meet your favorite internet superstar with the faceit app '
+  },
+  {
+    tag:'app',
+    img:'image',
+    title:'todo',
+    desc:'A todo app that features cloud sync with light and dark mode'
+  },
+  {
+    tag:'app',
+    img:'image',
+    title:'loopstudios',
+    desc:'A VR experience app made for Loopstudios'
+  },
+
 
 ]
 const Projects = () => {
-  return <div>
-    {projects.map((project, _index)=> <ProjectItem key={_index} {...project}/>)}
-    </div>
+  let params = useParams()
+  return <Container>
+    {projects.filter(project=>project.tag === params.design ).map((project, _index)=> <ProjectItem key={_index} {...project}/>)}
+  </Container>
 }
+
+const Container = styled.div`
+  display:flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+  gap:var(--location-gap);
+  margin:0 calc(max(2em, ( 100vw - var(--max-width) )/2 ));
+  margin-top:5em;
+
+  
+`
 
 export default Projects
