97fe529a177b106b19c53bed220cde79294969f4
Virus936 <lok.vira@gmail.com>
2022-02-03 18:53:49 +0100

    projet accompli

--- src/components/webdesign/WebDesignItem.js
+++ src/components/webdesign/WebDesignItem.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
+import {useNavigate} from 'react-router-dom'
 import {size} from '../../settings'
-import {  useNavigate } from "react-router-dom";
 
+const images = [
+  {
+    key:'graphic',
+    img:'image-graphic-design.jpg'
+  },
+  {
+    key:'app',
+    img:'image-app-design.jpg'
+  },
+  {
+    key:'web',
+    img:'image-web-design.jpg'
+  },
+]
 const src = document.location.origin
-const SkillItem = ({title, img, to}) => {
+const WebDesignItem = ({title,img,to}) => {
   const navigate = useNavigate()
+
   const handleNavigate = goto => {
     document.body.scrollTop = 0
     document.documentElement.scrollTop = 0
@@ -12,27 +27,37 @@ const SkillItem = ({title, img, to}) => {
   }
   return <Container>
     <img src={`${src}/asset/skill/bigscreen/${img}`}
-        srcSet={`${src}/asset/skill/mobile/${img} 450w, ./asset/skill/tablet/${img} 900w, ${src}/asset/skill/bigscreen/${img} 1900w `}         
-
         alt="" />
-    <h1> {title} </h1>
-    <span onClick={()=>handleNavigate(to)}>view projects</span>
 
-    </Container>
+    <h1> {title} </h1>
+    <span onClick={() => handleNavigate(`/${to}`)}>view projects</span>
+  </Container>
 }
-const Container = styled.article`
+
+const Container = styled.div`
   position:relative;
   display:flex;
-  overflow:hidden;
+  padding:90px;
   flex-direction:column;
-  user-select:none;
-
-  justify-content:center;
   align-items:center;
-  border-radius:var(--border-radius);
+  justify-content:center;
   color:white;
+  overflow:hidden;
+
+
+  @media(min-width:${size.small}){
+    border-radius:var(--border-radius);
+  }
+
+  @media(min-width:${size.medium}){
+    flex:1;
+  }
+
+
+  user-select:none;
   font-size:18px;
   text-transform:uppercase;
+  flex:90vw;
   &:hover{
     background-color:rgba(232, 128, 105, 0.8);
   }
@@ -40,7 +65,6 @@ const Container = styled.article`
     font-weight:700;
     letter-spacing:5px;
     font-size:1em;
-    
   }
   span{
     font-weight:700;
@@ -55,23 +79,8 @@ const Container = styled.article`
     object-fit:cover;
     z-index:-1;
     width:100%;
-    height:100%;
-    @media(min-width:450px){
-      height:110%;
-      width:auto;
-    }
   }
 
-  
-
-  &:first-child{
-    @media(min-width:${size.medium}){
-      grid-row:1/3;
-    }
-  }
-  &:nth-child(2){
-  }
-  &:nth-child(3){
-  }
 `
-export default SkillItem
+
+export default WebDesignItem
